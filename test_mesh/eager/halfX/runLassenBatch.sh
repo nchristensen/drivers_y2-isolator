a611f22c964924fad182ba0e92d7609204ffc9a6
Mike Anderson <manders2@illinois.edu>
2022-01-31 19:01:27 -0800

    added test mesh data sets for lazy parallel on bigger meshes

---
+++ test_mesh/eager/halfX/runLassenBatch.sh
@@ -0,0 +1,20 @@
+#! /bin/bash --login
+#BSUB -nnodes 4
+#BSUB -G uiuc
+#BSUB -W 720
+#BSUB -J Y2_iso_2d_halfX
+#BSUB -q pbatch
+#BSUB -o runOutput.txt
+#BSUB -e runOutput.txt
+
+module load gcc/7.3.1
+module load spectrum-mpi
+conda deactivate
+conda activate mirgeDriver.Y2isolator-parallel
+export PYOPENCL_CTX="port:tesla"
+#export PYOPENCL_CTX="0:2"
+jsrun_cmd="jsrun -g 1 -a 1 -n 16"
+export XDG_CACHE_HOME="/tmp/$USER/xdg-scratch"
+export POCL_CACHE_DIR_ROOT="/tmp/$USER/pocl-cache"
+$jsrun_cmd js_task_info
+$jsrun_cmd bash -c 'POCL_CACHE_DIR=$POCL_CACHE_DIR_ROOT/$$ python -O -u -m mpi4py ./isolator.py -i run_params.yaml --log  > mirge-1.out'
