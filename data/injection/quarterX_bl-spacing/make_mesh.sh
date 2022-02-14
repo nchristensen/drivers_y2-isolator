5a20f58bee7286c239ff81dc2ce920ca775946f7
Mike Anderson <anderson2981@gmail.com>
2022-02-14 16:17:12 -0600

    2d injector meshes

---
+++ data/injection/quarterX_bl-spacing/make_mesh.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gmsh -setnumber size 0.0032 -setnumber blratio 4 -setnumber blratiocavity 2 -setnumber blratioinjector 2 -setnumber injectorfac 10 -o isolator.msh -nopopup -format msh2 ./isolator.geo -2
