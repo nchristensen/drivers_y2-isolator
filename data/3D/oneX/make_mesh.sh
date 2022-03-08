d3f01ee3b75526946251d3a9448ed326277c3201
Mike Anderson <manders2@illinois.edu>
2022-03-08 10:01:12 -0800

    updates

--- data/3D/oneX/make_mesh.sh
+++ data/3D/oneX/make_mesh.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gmsh -setnumber size 0.0008 -setnumber blratio 1 -setnumber blratioc 1 -o isolator.msh -nopopup -format msh2 ./isolator.geo -3
+gmsh -setnumber size 0.0008 -setnumber blratio 1 -setnumber blratiocavity 1 -o isolator.msh -nopopup -format msh2 ./isolator.geo -3
