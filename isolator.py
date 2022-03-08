1c14eab6cbe2abaea21720f024b2f902404668d7
Mike Anderson <anderson2981@gmail.com>
2022-03-08 11:34:10 -0600

    update logging file destination

--- isolator.py
+++ isolator.py
@@ -733,8 +733,17 @@ def main(ctx_factory=cl.create_some_context, restart_filename=None,
         casename = "mirgecom"
 
     # logging and profiling
+    log_path = "log_data/"
+    logname = log_path + casename + ".sqlite"
+
+    if rank == 0:
+        import os
+        log_dir = os.path.dirname(logname)
+        if log_dir and not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
     logmgr = initialize_logmgr(use_logmgr,
-        filename=f"{casename}.sqlite", mode="wo", mpi_comm=comm)
+        filename=logname, mode="wo", mpi_comm=comm)
 
     if use_profiling:
         queue = cl.CommandQueue(cl_ctx,
