6ce56c63edd753e1e0f800a243d35d203f648e01
Mike Anderson <anderson2981@gmail.com>
2022-05-04 09:48:54 -0500

    cleaning up isolator CI

--- smoke_test_lazy/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#mpirun -n 1 python -u -O -m mpi4py isolator.py -i run_params.yaml --log --lazy 1>runOut.txt 2>runErr.txt
-mpirun -n 1 python -u -O -m mpi4py isolator.py -i run_params.yaml --log --lazy
