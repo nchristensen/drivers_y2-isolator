136cdd05f688c74f2c2a100afa547803f70166b8
Mike Anderson <manders2@illinois.edu>
2022-05-06 08:38:59 -0700

    lassen batch scripts

--- smoke_test_injection_2d/getTiming.sh
+++ smoke_test_injection_2d/getTiming.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
-avg_time=`grep "step walltime" mirge-1.out | awk 'NR>1 && NR!=20{ sum += $10 } END { if (NR > 0) print sum / NR }'`
-first_time=`grep "step walltime" mirge-1.out | awk 'NR==1{ sum += $10 } END { if (NR > 0) print sum }'`
-last_time=`grep "step walltime" mirge-1.out | awk 'NR==20{ sum += $10 } END { if (NR > 0) print sum }'`
+file="mirge-1.out"
+if [[ $# -gt "0" ]]; then
+  file=$1
+fi
+echo "Getting timing from ${file}"
+
+avg_time=`grep "step walltime" $file | awk 'NR>1 && NR!=20{ sum += $10 } END { if (NR > 0) print sum / NR }'`
+first_time=`grep "step walltime" $file | awk 'NR==1{ sum += $10 } END { if (NR > 0) print sum }'`
+last_time=`grep "step walltime" $file | awk 'NR==20{ sum += $10 } END { if (NR > 0) print sum }'`
 echo "first step time: $first_time"
 echo "last step time: $last_time"
 echo "average (2-20) step time: $avg_time"
